d29d7fd2ae146f8a8222d8248094a4853d8fc76c
cemil3535 <cemilozcan35@gmail.com>
2024-08-08 16:06:55 +0300

    Add project files.

---
+++ EncapsulationPractice/Araba.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncapsulationPractice
+{
+    internal class Araba
+    {
+        // field to identify
+        private string _brand;
+        private string _modal;
+        private string _color;
+        private int _doorNumber;
+
+        // doornumber property to identify
+        public int DoorNumber
+        {
+            get
+            {
+                return _doorNumber;
+            }
+            set  // If the number of doors is equal to 4 or 2, do  enter successfully. Otherwise, give a warning message and set the number of doors to -1.
+            {
+                if (value != 4 && value != 2)
+                {
+                    value = -1;
+                    _doorNumber = value;
+                    Console.WriteLine($"Kapi sayisi 2 yada 4 disinda bir deger alamaz. Kapi degeri: {_doorNumber}");
+                    
+                }
+
+                else
+                {
+
+                    Console.WriteLine("Kapi Girisi Basarili.");
+                }
+            }
+
+        }
+    }
+
+}
+
---
+++ EncapsulationPractice/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using EncapsulationPractice;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        // user get input
+        Console.WriteLine("Lutfen Kapi Sayisini Giriniz: ");
+        int doorNumber = Convert.ToInt32(Console.ReadLine());
+
+        // create car object
+        Araba araba = new Araba();
+        //door property value
+        araba.DoorNumber = doorNumber;
+        
+
+
+    }
+}
